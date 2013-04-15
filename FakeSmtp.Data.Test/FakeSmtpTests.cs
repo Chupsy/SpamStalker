e782ea1f3336e8b8ca4ed1fdef8160684f0c27b7
Chupsy <dufrasnes@intechinfo.fr>
2013-04-15 11:03:01 +0200

    Test NewMail

--- FakeSmtp.Data.Test/FakeSmtpTests.cs
+++ FakeSmtp.Data.Test/FakeSmtpTests.cs
@@ -13,23 +13,19 @@ using NUnit.Framework;
             [Test]
             public void MailIncomeToMailMessage()
             {
-                string 
-                MailTransfer NewMail = new MailTransfer;
-
+                string to = "tutu@intechinfo.fr";
+                string from = "boby@hotmail.net";
+                string body = "coucou comment ca va?";
+                string cc = "robbert@test.fr";
+                string subject = "coucou";
+                MailTransfer NewMail = new MailTransfer(to, from, cc, subject, body);
+                Assert.That(NewMail.to, Is.EqualTo("tutu@intechinfo.fr"));
+                Assert.That(NewMail.from, Is.EqualTo("boby@hotmail.net"));
+                Assert.That(NewMail.cc, Is.EqualTo("robbert@test.fr"));
+                Assert.That(NewMail.body, Is.EqualTo(body));
+                Assert.That(NewMail.subject, Is.EqualTo(subject));
             }
 
-            [Test]
-            public void SmtpClientObjectCreate()
-            {
-
-
-            }
-
-            [Test]
-            public void SmtpClientSendMessage()
-            {
-
-            }
         }
     }
 
