d5b60ae4cb8fa0c873537a676c761481efbb39b0
Chupsy <dufrasnes@intechinfo.fr>
2013-04-15 10:38:36 +0200

    Modification de Tests

--- FakeSmtp.Data.Test/FakeSmtpTests.cs
+++ FakeSmtp.Data.Test/FakeSmtpTests.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
-namespace FakeSmtp.Data.Test
-{
-    public class FakeSmtpTests
+    namespace FakeSmtp.Data.Tests
     {
+        [TestFixture]
+        public class FakeSmtpTest
+        {
+            [Test]
+            public void MailIncomeToMailMessage()
+            {
+
+            }
+
+            [Test]
+            public void SmtpClientObjectCreate()
+            {
+
+
+            }
+
+            [Test]
+            public void SmtpClientSendMessage()
+            {
+
+            }
+        }
     }
-}
+
+
