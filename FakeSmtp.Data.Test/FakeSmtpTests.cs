58cd5e145d806e79b2e42b337abfeb61af2d8439
Chupsy <dufrasnes@intechinfo.fr>
2013-04-15 10:26:13 +0200

    Rename of Test folder

--- FakeSmtp.Data.Test/FakeSmtpTests.cs
+++ FakeSmtp.Data.Test/FakeSmtpTests.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary1
+namespace FakeSmtp.Data.Test
 {
-    public class Class1
+    public class FakeSmtpTests
     {
     }
 }
