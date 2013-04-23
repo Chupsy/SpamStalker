e5177ecb50d393f989c46cfc81821b222bce1c80
Chupsy <dufrasnes@intechinfo.fr>
2013-04-23 16:59:44 +0200

    test martin

--- SMTPSupport.Test/SMTPSupportTests.cs
+++ SMTPSupport.Test/SMTPSupportTests.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace SMTPSupport.Test
 {
+    [TestFixture]
     public class SMTPSupportTests
     {
+        [Test]
+        public void testEHLO()
+        {
+
+        }
 
     }
 }
