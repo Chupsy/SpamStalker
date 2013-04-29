87edf42365896b0d334ea32a8ce87a5ee4416ae9
yopif <yopif@msn.com>
2013-04-29 09:42:04 +0200

    add RSET command

---
+++ SMTPSupport/RSETCommandToExecute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMTPSupport
+{
+    public class RSETCommandToExecute : SMTPCommandToExecute
+    {
+        string _domainName;
+
+        public RSETCommandToExecute()
+        {
+           
+        }
+
+        public override void Execute( SMTPSession session, SMTPCallingClient client )
+        {
+            if( session.IsInitialized )
+            {
+                session.Clear();
+                client.SendSuccess();
+            }
+            else
+            {
+                client.SendError(500);
+            }
+        }
+
+    }
+
+}
