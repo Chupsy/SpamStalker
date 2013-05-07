063a2125db02ebd61ba85ba430b2d25cf9221694
yopif <yopif@msn.com>
2013-05-07 12:06:06 +0200

    windows form : selection d'adresse et affichage de la blacklist

--- ClientWindow/Form1.cs
+++ ClientWindow/Form1.cs
@@ -12,9 +12,25 @@ namespace ClientWindow
 {
     public partial class Form1 : Form
     {
+        string[] fromAddresses = System.IO.File.ReadAllLines(@"..\..\..\FakeSmtp\senderAdresses.txt");
+        string[] toAddresses = System.IO.File.ReadAllLines(@"..\..\..\FakeSmtp\adresses.txt");
+        List<string> _blackList = new List<string>();
+       
         public Form1()
         {
             InitializeComponent();
+
+            foreach (string adress in fromAddresses)
+            {
+                _blackList.Add(adress);
+            }
+            listBox1.DataSource = _blackList;
+
+            foreach (string adress in toAddresses)
+            {
+                comboBox1.Items.Add(adress);
+            }
+            comboBox1.SelectedIndex = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
