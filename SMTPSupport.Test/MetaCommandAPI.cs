988237ead2b5955c48efa0397eae24e2c115596c
Snasch <martin.lavie.pro@gmail.com>
2013-05-30 11:16:14 +0200

    [Fix]
    Implémentation de MetaCommandAPI dans les test

---
+++ SMTPSupport.Test/MetaCommandAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMTPSupport;
+
+namespace FakeSmtp
+{
+    class MetaCommandAPI : IMetaCommandAPI
+    {
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Resume()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public ServerStatus Status
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
