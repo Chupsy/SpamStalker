25370ec05219be4436cc0cad620ffab8c419b99f
Snasch <martin.lavie.pro@gmail.com>
2013-06-10 11:22:14 +0200

    [ADD]
    CreateUser

--- FakeSmtp/User.cs
+++ FakeSmtp/User.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataSupport;
+using System.Configuration;
+using System.IO;
 
 namespace FakeSmtp
 {
@@ -17,6 +19,39 @@ namespace FakeSmtp
         public User(string username, string password, string accountType)
         {
             _username = username;
+        }
+
+        static bool CreateUser(string username, string password, string newAdress, string AccountType)
+        {
+            string _dataPath = ConfigurationManager.AppSettings["dataDirectory"];
+            _dataPath = _dataPath + "\\User\\";
+            string _description = "Main adress";
+            string fileUser = _dataPath + username + ".txt";
+            string _passLine = "Password = " + password;
+            string _accountTypeLine = "Account = " + AccountType;
+
+            if (!Directory.Exists(_dataPath))
+            {
+                Directory.CreateDirectory(_dataPath);
+                File.Create(fileUser);
+            }
+            else if (!File.Exists(fileUser))
+            {
+                File.Create(fileUser);
+            }
+
+            StreamWriter stream = new StreamWriter(@fileUser);
+            stream.WriteLine(_passLine);
+            stream.WriteLine(_accountTypeLine);
+            stream.Close();
+
+            AddAdress(newAdress, _description, newAdress);
+            return true;
+        }
+
+        static void AddAdress(string adress, string description, string relayAdress)
+        {
+            
         }
     }
 }
