876eaee718d4071c6f31bda6ea9ac85d2a9832a9
yopif <yopif@msn.com>
2013-06-10 15:47:49 +0200

    greza

--- ClientWindow/Form1.Designer.cs
+++ ClientWindow/Form1.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
@@ -60,17 +60,17 @@
             // 
             // listBox1
             // 
-            this.listBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.listBox1.BackColor = System.Drawing.Color.White;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 61);
+            this.listBox1.Location = new System.Drawing.Point(12, 65);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(432, 368);
+            this.listBox1.Size = new System.Drawing.Size(432, 355);
             this.listBox1.TabIndex = 2;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(420, 442);
+            this.button2.Location = new System.Drawing.Point(403, 442);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -79,62 +79,53 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(386, 442);
+            this.button3.BackgroundImage = global::ClientWindow.Properties.Resources.poubelle_bouton2;
+            this.button3.Location = new System.Drawing.Point(315, 435);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(28, 23);
+            this.button3.Size = new System.Drawing.Size(39, 38);
             this.button3.TabIndex = 4;
-            this.button3.Text = "X";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(401, 21);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "EHLO tutu";
-            this.button4.UseCompatibleTextRendering = true;
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click_1);
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(450, 61);
+            this.listBox2.Location = new System.Drawing.Point(450, 65);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(26, 368);
+            this.listBox2.Size = new System.Drawing.Size(26, 355);
             this.listBox2.TabIndex = 6;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(288, 442);
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Image = global::ClientWindow.Properties.Resources.medium_doigt_d_honneur_outon;
+            this.button5.Location = new System.Drawing.Point(266, 435);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(43, 38);
             this.button5.TabIndex = 7;
-            this.button5.Text = "Fuck";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(197, 442);
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Image = global::ClientWindow.Properties.Resources.no_doigt_d_honneur_outon;
+            this.button6.Location = new System.Drawing.Point(216, 435);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(44, 38);
             this.button6.TabIndex = 8;
-            this.button6.Text = "Unfuck";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 477);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(497, 482);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
@@ -154,7 +145,6 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
